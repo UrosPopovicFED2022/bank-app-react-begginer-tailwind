17fdf90b051385574419f3abb06e53389194341e
Uros Popovic <uros32popovic@gmail.com>
2022-12-01 16:00:01 +0100

    begginer project

---
+++ modern-bank-app/src/components/index.js
@@ -0,0 +1,29 @@
+import Billing from "./Billing";
+import Business from "./Business";
+import Button from "./Button";
+import CardDeal from "./CardDeal";
+import Clients from "./Clients";
+import Cta from "./Cta";
+import FeedbackCard from "./FeedbackCard";
+import Footer from "./Footer";
+import GetStarted from "./GetStarted";
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import Stats from "./Stats";
+import Testimonials from "./Testimonials";
+
+export {
+  Button,
+  Navbar,
+  Billing,
+  FeedbackCard,
+  CardDeal,
+  GetStarted,
+  Business,
+  Clients,
+  Cta,
+  Stats,
+  Footer,
+  Testimonials,
+  Hero,
+};
